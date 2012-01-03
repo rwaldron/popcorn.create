901fe8295eb944660323ce9e269a5e7f19c3365d
Rick Waldron <waldron.rick@gmail.com>
2012-01-03 13:49:09 -0500

    Drops support for old Popcorn player APIs

--- popcorn.create.js
+++ popcorn.create.js
@@ -1,80 +1,66 @@
-//  Requires Popcorn.js
+//	Requires Popcorn.js
 (function( global, Popcorn ) {
-  
-  var doc = global.document;
 
-  Popcorn.create = function( options ) {
-    return new Popcorn.create.init( options );
-  };
- 
-  Popcorn.create.init = function( options ) {
+	var doc = global.document;
 
-		// If a player has been specified and that player exists and a src exists
-		// create the new popcorn object with specified player plugin
-		if ( options.player && Popcorn[ options.player ] && options.src ) {
-			
-			// Return fully created Popcorn media object
-			return Popcorn(
-				      	Popcorn[ options.player ]( 
-									options.placeholder, 
-									options.src, 
-									options.setup || {} 
-								)
-							);
-			
-		} else {
-			
-			// Create new video element 
-			// TODO: modify to accept either audio or video 
-	    var media = doc.createElement( options.type || "video" ), 
-	        guid = Popcorn.guid( "__popcorn" ), 
-					pop;
+	Popcorn.create = function( options ) {
+		return new Popcorn.create.init( options );
+	};
 
-	    options.id = options.id || guid;
+	Popcorn.create.init = function( options ) {
 
-	    Popcorn.extend( media, options );
+		var media, guid, pop;
 
-	    media.style.display = "none";
+		// Create new video element
+		// TODO: modify to accept either audio or video
+		media = doc.createElement( options.type || "video" );
+		guid = Popcorn.guid( "__popcorn" );
 
-	    doc.body.appendChild( media );
-			
-			pop = Popcorn( "#" + options.id );
-			// If a placeholder was specified, 
-			// immediately append new media 
-			if ( options.placeholder ) {
-				pop.appendTo( 
-					( options.placeholder[0] !== "#" ? "#" : "" ) + 
-					options.placeholder
-				);
+
+		options.id = options.id || guid;
+
+		Popcorn.extend( media, options );
+
+		media.style.display = "none";
+
+		doc.body.appendChild( media );
+
+		pop = Popcorn( "#" + options.id );
+		// If a placeholder was specified,
+		// immediately append new media
+		if ( options.placeholder ) {
+			pop.appendTo(
+				( options.placeholder[0] !== "#" ? "#" : "" ) +
+				options.placeholder
+			);
+		}
+
+		return pop;
+	};
+
+	//	Popcorn.create() returns a popcorn object
+
+	Popcorn.extend( Popcorn.p, {
+		appendTo: function( parent ) {
+
+			var matches = doc.querySelectorAll( parent ),
+					orig = doc.getElementById( this.media.id ),
+					clone = this.media.cloneNode( true );
+
+			if ( matches.length ) {
+
+				matches[ 0 ].appendChild( clone );
+
+				this.media = clone;
+
+				this.media.style.display = "";
+
+				orig.parentNode.removeChild( orig );
 			}
 
-	    return pop;
-		}
-  };
-  
-  //  Popcorn.create() returns a popcorn object
-  
-  Popcorn.extend( Popcorn.p, {
-    appendTo: function( parent ) {
-      
-      var matches = doc.querySelectorAll( parent ), 
-          orig = doc.getElementById( this.media.id ), 
-          clone = this.media.cloneNode( true );
-      
-      if ( matches.length ) {
-        
-        matches[ 0 ].appendChild( clone );
-        
-        this.media = clone;
-        
-        this.media.style.display = "";
-        
-        orig.parentNode.removeChild( orig );
-      }
-			
 			// Allow appendTo() to chain
 			return this;
-    }
-  });
+		}
+	});
 
 })( this, Popcorn );
--- test/popcorn.create.unit.js
+++ test/popcorn.create.unit.js
@@ -1,8 +1,8 @@
 var mediaList = [
-      "assets/snowdriving.ogv",
-      "assets/eich.ogv",
-      "assets/crockford.ogv"
-    ];
+			"assets/snowdriving.ogv",
+			"assets/eich.ogv",
+			"assets/crockford.ogv"
+		];
 
 function clear(id) {
 	document.getElementById(id).innerHTML = "";
@@ -11,198 +11,198 @@ function clear(id) {
 
 module("API");
 test("Popcorn.create", function() {
-  
-  expect(2);
-  
-  ok( Popcorn.create, "Popcorn.create exists" );
-  
-  equal( typeof Popcorn.create, "function", "Popcorn.create() is a function" );
-  
+
+	expect(2);
+
+	ok( Popcorn.create, "Popcorn.create exists" );
+
+	equal( typeof Popcorn.create, "function", "Popcorn.create() is a function" );
+
 });
 
 test("Popcorn.prototype.appendTo()", function() {
-  
-  var expects = 3, 
-      count = 0;
 
-  expect(expects);
-  
-  function plus() { 
-    
-    if ( ++count === expects ) {
-      start();    
+	var expects = 3,
+			count = 0;
+
+	expect(expects);
+
+	function plus() {
+
+		if ( ++count === expects ) {
+			start();
 			clear("unmoved-fixture");
-    }
-  }  
-  
-  stop();
+		}
+	}
 
-	ok( Popcorn.prototype.appendTo, "Popcorn.prototype.appendTo is defined" );    
+	stop();
+
+	ok( Popcorn.prototype.appendTo, "Popcorn.prototype.appendTo is defined" );
 	plus();
-	
+
 	equal( typeof Popcorn.prototype.appendTo, "function", "Popcorn.prototype.appendTo is a function" );
-  plus();
+	plus();
 
-  Popcorn.create({
+	Popcorn.create({
 
-    src: "assets/snowdriving.ogv"
+		src: "assets/snowdriving.ogv"
 
-  }).listen( "loadedmetadata", function() {
-    
+	}).listen( "loadedmetadata", function() {
+
 		ok( this.appendTo, "`this` popcorn object has inherited `appendTo`" );
 		plus();
-    
-  });
-  
+
+	});
+
 });
 
 
 module("Usage");
 test("create( options, no placeholder )", function() {
 
-  var expects = 2, 
-      count = 0;
+	var expects = 2,
+			count = 0;
 
-  expect(expects);
-  
-  function plus() { 
-    
-    if ( ++count === expects ) {
-      start();    
-			
+	expect(expects);
+
+	function plus() {
+
+		if ( ++count === expects ) {
+			start();
+
 			clear("unmoved-fixture");
-    }
-  }  
-  
-  stop();
-  
-  Popcorn.create({
-  
-    src: "assets/snowdriving.ogv",
-    controls: true
-  
-  }).listen( "loadedmetadata", function() {
-    
-    ok( true, "loadedmetadata fired on hidden video element" );
-    plus();
-    
-    this.appendTo( "#unmoved-fixture" );
-    
-    
-    equal( document.getElementById( "unmoved-fixture" ).childNodes.length, 1, "#unmoved-fixture as 1 childNode" );
-    plus();
-  
-  });
+		}
+	}
+
+	stop();
+
+	Popcorn.create({
+
+		src: "assets/snowdriving.ogv",
+		controls: true
+
+	}).listen( "loadedmetadata", function() {
+
+		ok( true, "loadedmetadata fired on hidden video element" );
+		plus();
+
+		this.appendTo( "#unmoved-fixture" );
+
+
+		equal( document.getElementById( "unmoved-fixture" ).childNodes.length, 1, "#unmoved-fixture as 1 childNode" );
+		plus();
+
+	});
 
 });
 
 test("create( options, w/ placeholder string )", function() {
 
-  var expects = 2, 
-      count = 0;
+	var expects = 2,
+			count = 0;
 
-  expect(expects);
-  
-  function plus() { 
-    
-    if ( ++count === expects ) {
-      start(); 
+	expect(expects);
+
+	function plus() {
+
+		if ( ++count === expects ) {
+			start();
 			clear("unmoved-fixture");
-    }
-  }  
-  
-  stop();
-  
-  Popcorn.create({
-  
-    src: "assets/snowdriving.ogv",
+		}
+	}
+
+	stop();
+
+	Popcorn.create({
+
+		src: "assets/snowdriving.ogv",
 		placeholder: "unmoved-fixture",
-    controls: true
-  
-  }).listen( "loadedmetadata", function() {
-    
-    ok( true, "loadedmetadata fired on visible video element" );
-    plus();
-    
-    equal( document.getElementById( "unmoved-fixture" ).childNodes.length, 1, "#unmoved-fixture as 1 childNode" );
-    plus();
-  
-  });
+		controls: true
+
+	}).listen( "loadedmetadata", function() {
+
+		ok( true, "loadedmetadata fired on visible video element" );
+		plus();
+
+		equal( document.getElementById( "unmoved-fixture" ).childNodes.length, 1, "#unmoved-fixture as 1 childNode" );
+		plus();
+
+	});
 
 });
 
 
 test("create( options, w/ placeholder selector )", function() {
 
-  var expects = 2, 
-      count = 0;
+	var expects = 2,
+			count = 0;
 
-  expect(expects);
-  
-  function plus() { 
-    
-    if ( ++count === expects ) {
-      start();                 
+	expect(expects);
+
+	function plus() {
+
+		if ( ++count === expects ) {
+			start();
 			clear("unmoved-fixture");
-    }
-  }  
-  
-  stop();
-  
-  Popcorn.create({
-  
-    src: "assets/snowdriving.ogv",
-		placeholder: "#unmoved-fixture",
-    controls: true
-  
-  }).listen( "loadedmetadata", function() {
-    
-    ok( true, "loadedmetadata fired on visible video element" );
-    plus();
-    
-    equal( document.getElementById( "unmoved-fixture" ).childNodes.length, 1, "#unmoved-fixture as 1 childNode" );
-    plus();
-  
-  });
-
-});
-
-
-test("create( with specified player )", function() {
-
-  var expects = 2, 
-      count = 0;
-
-  expect(expects);
-  
-  function plus() { 
-    
-    if ( ++count === expects ) {
-      start(); 
-    }
-  }  
-  
-  stop();
-  
-  Popcorn.create({
-		
-		player: "vimeo",
-		
-		// args to Popcorn.vimeo()
-		placeholder: "vimeo-fixture", 
-		src: "http://vimeo.com/22444635",
-		setup: {
-			// player settings here
-			// TODO: this is currently untested
 		}
-  
-  }).listen( "loadedmetadata", function() {
-    
-		ok( true, "loadedmetadata fired on vimeo element" );
-    plus();
+	}
 
-    equal( document.getElementById( "vimeo-fixture" ).nodeName, "OBJECT", "#vimeo-fixture was converted into an &lt;object&gt; element" );
-    plus();
-  });
+	stop();
+
+	Popcorn.create({
+
+		src: "assets/snowdriving.ogv",
+		placeholder: "#unmoved-fixture",
+		controls: true
+
+	}).listen( "loadedmetadata", function() {
+
+		ok( true, "loadedmetadata fired on visible video element" );
+		plus();
+
+		equal( document.getElementById( "unmoved-fixture" ).childNodes.length, 1, "#unmoved-fixture as 1 childNode" );
+		plus();
+
+	});
 
 });
+
+
+// test("create( with specified player )", function() {
+//
+// 	var expects = 2,
+// 			count = 0;
+//
+// 	expect(expects);
+//
+// 	function plus() {
+//
+// 		if ( ++count === expects ) {
+// 			start();
+// 		}
+// 	}
+//
+// 	stop();
+//
+// 	Popcorn.create({
+//
+// 		player: "vimeo",
+//
+// 		// args to Popcorn.vimeo()
+// 		placeholder: "vimeo-fixture",
+// 		src: "http://vimeo.com/22444635",
+// 		setup: {
+// 			// player settings here
+// 			// TODO: this is currently untested
+// 		}
+//
+// 	}).listen( "loadedmetadata", function() {
+//
+// 		ok( true, "loadedmetadata fired on vimeo element" );
+// 		plus();
+//
+// 		equal( document.getElementById( "vimeo-fixture" ).nodeName, "OBJECT", "#vimeo-fixture was converted into an &lt;object&gt; element" );
+// 		plus();
+// 	});
+//
+// });
